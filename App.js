2cf79598747514e69bf51352d5958c113e7175c0
PAN Zhiqi <ericpoon510@gmail.com>
2018-03-06 12:39:29 +0800

    Set up nested navigators

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 
 import AuthScreen from './src/screens/AuthScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
+import MapScreen from './src/screens/MapScreen';
+import DeckScreen from './src/screens/DeckScreen';
+import ReviewScreen from './src/screens/ReviewScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
 
 export default class App extends React.Component {
   render() {
-    const MainNavigator = TabNavigator({
-      welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen },
+    const ReviewStackNavigator = StackNavigator({
+      review: ReviewScreen,
+      settings: SettingsScreen,
+    });
+    const MainTabNavigator = TabNavigator({
+      map: MapScreen,
+      deck: DeckScreen,
+      review: ReviewStackNavigator,
+    });
+    const RootTabNavigator = TabNavigator({
+      welcome: WelcomeScreen,
+      auth: AuthScreen,
+      main: MainTabNavigator,
     });
 
     return (
-      <MainNavigator />
+      <RootTabNavigator />
     );
   }
 }
