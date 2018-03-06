25bf60a0133e17716ee0b0798e1f36fccc42cbc6
PAN Zhiqi <ericpoon510@gmail.com>
2018-03-06 15:35:33 +0800

    Able to navigate from slides to auth screen

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
+  renderEnterButton = () => {
+    const { enterButtonText, onComplete } = this.props;
+    return (
+      <Button
+        title={enterButtonText}
+        raised
+        buttonStyle={styles.enterButton}
+        containerViewStyle={styles.enterButtonContainer}
+        onPress={onComplete}
+      />
+    );
+  };
+
   renderSlides = () => {
-    return this.props.data.map(slide => {
+    const { data } = this.props;
+    return data.map((slide, index) => {
       return (
         <View
           key={slide.text}
           style={[styles.slide, { backgroundColor: slide.color }]}
         >
           <Text style={styles.slideText}>{slide.text}</Text>
+          {index === data.length - 1 && this.renderEnterButton()}
         </View>
       );
     });
@@ -42,6 +58,12 @@ const styles = {
     textAlign: 'center',
     padding: 10,
     color: 'white',
+  },
+  enterButton: {
+    backgroundColor: '#1d8ae8',
+  },
+  enterButtonContainer: {
+    marginTop: 20,
   },
 };
 
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -5,13 +5,21 @@ import Slides from '../components/Slides';
 const SLIDE_DATA = [
   { text: 'Welcome to JobApp', color: '#24d7ba' },
   { text: 'Set your location, then swipe away', color: '#3fbcff' },
-  { text: 'Land to your dream job', color: '#7e7fea' },
+  { text: 'Land to your dream job', color: '#ea8b2c' },
 ];
 
 class WelcomeScreen extends Component {
+  onSlidesComplete = () => {
+    this.props.navigation.navigate('auth');
+  };
+
   render() {
     return (
-      <Slides data={SLIDE_DATA} />
+      <Slides
+        data={SLIDE_DATA}
+        enterButtonText={'Start Exploring!'}
+        onComplete={this.onSlidesComplete}
+      />
     );
   }
 }
