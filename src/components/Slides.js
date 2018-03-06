24e0a0445549a1d355c540344d364e471d7390f9
PAN Zhiqi <ericpoon510@gmail.com>
2018-03-06 15:11:43 +0800

    Add slides in welcome screen

---
+++ src/components/Slides.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+class Slides extends Component {
+  renderSlides = () => {
+    return this.props.data.map(slide => {
+      return (
+        <View
+          key={slide.text}
+          style={[styles.slide, { backgroundColor: slide.color }]}
+        >
+          <Text style={styles.slideText}>{slide.text}</Text>
+        </View>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <ScrollView
+        horizontal
+        pagingEnabled
+        style={{ flex: 1 }}
+      >
+        {this.renderSlides()}
+      </ScrollView>
+    );
+  }
+}
+
+const styles = {
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: SCREEN_WIDTH,
+  },
+  slideText: {
+    fontSize: 30,
+    textAlign: 'center',
+    padding: 10,
+    color: 'white',
+  },
+};
+
+export default Slides;
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  { text: 'Welcome to JobApp', color: '#24d7ba' },
+  { text: 'Set your location, then swipe away', color: '#3fbcff' },
+  { text: 'Land to your dream job', color: '#7e7fea' },
+];
 
 class WelcomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-      </View>
+      <Slides data={SLIDE_DATA} />
     );
   }
 }
